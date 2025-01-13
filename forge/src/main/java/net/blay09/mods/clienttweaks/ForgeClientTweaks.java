df44f063231c4122b71b5912783972055965af0b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-13 20:23:52 +0100

    feat: Update to Forge 1.21.4

--- forge/src/main/java/net/blay09/mods/clienttweaks/ForgeClientTweaks.java
+++ forge/src/main/java/net/blay09/mods/clienttweaks/ForgeClientTweaks.java
@@ -1,22 +1,24 @@
 package net.blay09.mods.clienttweaks;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(ClientTweaks.MOD_ID)
 public class ForgeClientTweaks {
 
-    public ForgeClientTweaks() {
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> Balm.initialize(ClientTweaks.MOD_ID, EmptyLoadContext.INSTANCE, ClientTweaks::initializeCommon));
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(ClientTweaks.MOD_ID, EmptyLoadContext.INSTANCE, ClientTweaks::initializeClient));
+    public ForgeClientTweaks(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        if (FMLEnvironment.dist.isClient()) {
+            Balm.initialize(ClientTweaks.MOD_ID, loadContext, ClientTweaks::initializeCommon);
+            BalmClient.initialize(ClientTweaks.MOD_ID, loadContext, ClientTweaks::initializeClient);
+        }
 
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
+        context.registerDisplayTest(IExtensionPoint.DisplayTest.IGNORE_ALL_VERSION);
     }
 
 }
